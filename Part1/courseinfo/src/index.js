72d5f78d0f4fc4573be4cdb71ff1ff0aaf6dd548
natnael1455 <33367817+natnael1455@users.noreply.github.com>
2020-08-26 05:14:20 +0300

    ex 1.5

--- Part1/courseinfo/src/index.js
+++ Part1/courseinfo/src/index.js
@@ -32,27 +32,30 @@ const Total = (props) =>{
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
     <div>
-      <Header course_name = {course} />
-      <Content parts_a = {parts} />
-      <Total total_value = {parts} />
+      <Header course_name = {course.name} />
+      <Content parts_a = {course.parts} />
+      <Total total_value = {course.parts} />
     </div>
   )
 }
