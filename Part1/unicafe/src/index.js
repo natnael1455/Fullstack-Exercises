8fa30d93a9041bee1541a3022a05aed2fd5abd40
natnael1455 <33367817+natnael1455@users.noreply.github.com>
2020-10-22 21:58:07 +0300

    ex 1.7

--- Part1/unicafe/src/index.js
+++ Part1/unicafe/src/index.js
@@ -20,16 +20,38 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  
+  // function calling button even hander
+  const  onClick = (button)=>{
+    const handler =()=>{
+      if(button == "good"){
+        setGood(good+1)
+      }
+      else if (button == "neutral"){
+        setNeutral(neutral+1)
+      }
+      else {
+        setBad(bad+1)
+      }
+      setAll(all +1)
+    }
+    return handler
+  }
+ 
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button text='good' action= {() => setGood(good+1)}/> <Button text='neutral' action= {() => setNeutral(neutral+1)}/> 
-      <Button text='bad' action= {() => setBad(bad+1)}/>
+      <Button text='good' action= {onClick('good')}/> <Button text='neutral' action= {onClick('neutral')}/> <Button text='bad' action= {onClick('bad')}/>
       <h1>stastics</h1>
         <Stat name='good' result={good} />
         <Stat name='neutral' result={neutral} />
         <Stat name='bad' result={bad} />
+        <Stat name='all' result={all} />
+        <Stat name='averge' result={((good*1)+(bad*-1))/all} />
+        <Stat name='positive' result={ (good/all*100) + '%'} />
+        
     </div>
   )
 }
