7adde245d5ac25ee14f3a0f4c2f8a3a7eb668330
natnael1455 <33367817+natnael1455@users.noreply.github.com>
2020-10-24 01:18:21 +0300

    ex end

--- Part1/anecdotes/src/index.js
+++ Part1/anecdotes/src/index.js
@@ -1,18 +1,50 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = ({ text, action }) =>{
+  return (
+  <button onClick={action}>
+    {text}
+  </button>
+  )
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState({vote:[0,0,0,0,0,0],mostvote:0})
 
+  
   const clickhandler = () =>{
     let i =Math.floor(Math.random() * 6)
     setSelected(i)
   }
 
+  const click_vote =()=>{
+    const copy = { ...votes }
+    copy.vote[selected]+=1
+    if (copy.vote[selected] > copy.vote[copy.mostvote]){
+      copy.mostvote=selected
+    }
+    else {
+      copy.mostvote=votes.mostvote
+    }
+      
+    setVotes(copy)
+  }
+
+  
+
   return (
     <div>
-      {props.anecdotes[selected]} <br/>
-      <button onClick={clickhandler}>next anecdotes</button>
+      <h1>Anecdote of the day</h1>
+
+     <p> {props.anecdotes[selected]} </p>
+     <p>it has {votes.vote[selected]} votes </p>
+
+     <p> <Button text='vote' action={click_vote}/> <Button text=' next anecdote' action={clickhandler} /></p>
+     <h1>Anecdote with the most votes</h1>
+     <p> {props.anecdotes[votes.mostvote]} </p>
+     <p>it has {votes.vote[votes.mostvote]} votes </p>
     </div>
   )
 }
