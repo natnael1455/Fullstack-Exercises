45539be6b5366da89991bc303bda91baeb83fceb
natnael1455 <33367817+natnael1455@users.noreply.github.com>
2020-11-19 17:26:51 +0200

    ex 2.10

--- Part 2/phonebook/src/components/App.js
+++ Part 2/phonebook/src/components/App.js
@@ -1,9 +1,54 @@
 import React, { useState } from 'react'
 
+const Form = ({addName,newName,handleNameChange,newNumber,handleNumberChange}) =>{
+  return(
+    <div>
+    <h2>Add a New </h2>
+    <form onSubmit={addName}>
+        <div> name: <input value={newName} onChange={handleNameChange}/></div>
+        <div> number: <input value={newNumber} onChange={handleNumberChange}/></div>
+        
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+  </div>
+  )
+}
+const Detail = ({person}) => {
+  return (
+    <p>{person.name} : {person.number}</p>
+  )
+}
+const Persons = ({personToShow}) => {
+  return(
+    <div>
+     <h2>Numbers</h2>
+        {personToShow.map(person =><Detail key={person.name} person={person} />)}
+   </div>
+  )
+}
+
+const Filter = ({showAll,handleShowChange}) =>{
+  return(
+    <div>
+    <h2>Phonebook</h2>
+    filter shown with <input value={showAll} onChange={handleShowChange}/>
+    </div>
+  )
+}
 const App = () => {
-  const [ persons, setPersons ] = useState([ { name: 'Arto Hellas', number:'045978612' }]) 
+
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
   const [ newName, setNewName ] = useState('enter the name')
   const [newNumber,setNewNumber] = useState('000')
+  const [showAll, setShowAll] = useState('')
+
   const addName = (event) => {
     event.preventDefault()
     const same = (element) => element  === newName;
@@ -33,19 +78,21 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleShowChange = (event) => {
+    console.log(event.target.value)
+    setShowAll(event.target.value)
+  }
+
+  const personToShow = (showAll==='')
+  ? persons
+  : persons.filter(person => person.name.toLowerCase().includes(showAll.toLocaleLowerCase()))
+
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addName}>
-        <div> name: <input value={newName} onChange={handleNameChange}/></div>
-        <div> number: <input value={newNumber} onChange={handleNumberChange}/></div>
-        
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-        {persons.map(person =><p key={person.name}>{person.name} : {person.number}</p>)}
+      <Filter showAll={showAll} handleShowChange={handleShowChange} />
+      <Form addName={addName} newName={newName} handleNameChange ={handleNameChange} newNumber={newNumber}
+        handleNumberChange={handleNumberChange} />
+      <Persons personToShow={personToShow} />
     </div>
 
     
