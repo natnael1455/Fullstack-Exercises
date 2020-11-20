17fe12f913745d8dc4ab4d0df7b6a6f75802a01a
natnael1455 <33367817+natnael1455@users.noreply.github.com>
2020-11-20 14:39:47 +0200

    ex 2.11

--- Part 2/phonebook/src/components/App.js
+++ Part 2/phonebook/src/components/App.js
@@ -1,20 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import Form from './Form'
 import Persons from './Persons'
 import Filter from './Filter'
+import axios from 'axios'
 
 const App = () => {
 
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('enter the name')
   const [newNumber,setNewNumber] = useState('000')
   const [showAll, setShowAll] = useState('')
-
+  
+  useEffect(() => {
+    axios
+      .get(' http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+  
   const addName = (event) => {
     event.preventDefault()
     const same = (element) => element  === newName;
