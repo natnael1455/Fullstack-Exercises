0ade9dd3c752e63d523652fe0cb3c0ce5b199b1a
natnael1455 <33367817+natnael1455@users.noreply.github.com>
2020-11-21 05:15:01 +0200

    2.18 last

--- Part 2/phonebook/src/services/person.js
+++ Part 2/phonebook/src/services/person.js
@@ -8,16 +8,28 @@ const getAll = () => {
 
  const create = newObject => {
   const request = axios.post(baseUrl, newObject)
- return request.then(response => response.data)
+ return request
+ .then(response => response.data)
+ .catch(error => {
+  alert("not created");
+})
 }
 
 const update = (id, newPerson) => {
     const request = axios.put(`${baseUrl}/${id}`, newPerson)
-    return request.then(response => response.data)
+    return request
+    .then(response => response.data)
+    .catch(error => {
+      alert("not updated");
+    })
   }
 const delet = (id) => {
   const request = axios.delete(`${baseUrl}/${id}`)
-  return request.then(response => response.data)
+  return request
+  .then(response => response.data)
+  .catch(error => {
+    alert("not deleted");
+  })
 }
 
 export default { getAll,create,delet,update}
